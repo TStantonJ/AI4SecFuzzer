0dbed5354bae39b790f354bff15281fcd8480a65
ThomasRStanton <thomasrstantonjr@gmail.com>
2022-04-08 16:06:14 -0500

    Added box and whisker plot

--- programDriver.py
+++ programDriver.py
@@ -1,11 +1,15 @@
 from re import L
+
+from numpy import average
 from fuzzer import main as fuzz
 import matplotlib.pyplot as plt
 import time
+import statistics
+import numpy as np
 
 
-NUMBER_OF_EVALS  = 300
-NUMBER_OF_RUNS = 30
+NUMBER_OF_EVALS  = 30
+NUMBER_OF_RUNS = 5
 
 best_run_holder = []        # Holds list of best_eval_holders
 general_run_holder = []     # Holds list of results of each eval per run
@@ -23,13 +27,13 @@ for run in range(NUMBER_OF_RUNS):
     for eval in range(NUMBER_OF_EVALS):
         # Run fuzzer
         print('\t\t\t\t\tCurrent Eval :',eval, end='\r')
-        time.sleep(0.01)
+        time.sleep(0.001)
         result = fuzz()
         # Update best eval so far
         if result >= best_eval:
             best_eval = result
         best_eval_holder.append(best_eval)
-        # Add eval to data from this run
+        # Always add eval to data from this run
         general_eval_holder.append(result)
     print('\n')
 
@@ -42,12 +46,24 @@ for run in range(NUMBER_OF_RUNS):
     best_run_holder.append(best_eval_holder)
     general_run_holder.append(general_eval_holder)
 
-#Plot resutls of all runs
-for run in best_run_holder:
-    xpoints = range(NUMBER_OF_EVALS)
-    ypoints = run
-    plt.plot(xpoints, ypoints)
 
+#Plot resutls of all runs
+
+# Graph of best fitness seen by eval for all runs
+for run in best_run_holder:
+    bestxpoints = range(NUMBER_OF_EVALS)
+    bestypoints = run
+    plt.plot(bestxpoints, bestypoints)
+# Box plot graph
+data_holder = []
+for eval_num in range(NUMBER_OF_EVALS):
+    average_at_eval = []
+    for run in range(len(general_run_holder)):
+        average_at_eval.append(general_run_holder[run][eval_num])
+    data_holder.append(average_at_eval)
+fig, ax = plt.subplots()
+ax.boxplot(data_holder)
+# Show graphs
 plt.show()
 
 print('Best Fitness:', best_fitness)
