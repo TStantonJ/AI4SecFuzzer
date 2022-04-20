32b63db6ed8f85c06eb0846bd133631d1ede0e60
ThomasRStanton <thomasrstantonjr@gmail.com>
2022-04-20 15:00:01 -0500

    Added capitol letters to mutateString()

--- neighborhood.py
+++ neighborhood.py
@@ -79,7 +79,7 @@ def mutateString(_string):
     symb_chance = 0.33
 
     alphabet_cost = 1
-    alphabet_chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    alphabet_chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
     number_cost = 2
     number_chars = [1,2,3,4,5,6,7,8,9,0]
