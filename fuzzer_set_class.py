05c90dca4e543e11ef70aab8ce1a4d5d3761f754
CarloBaylon <56897130+CarloBaylon@users.noreply.github.com>
2022-04-26 12:37:56 -0500

    Add files via upload

---
+++ fuzzer_set_class.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass, field, asdict
+
+
+@dataclass
+class fuzzer_set:
+    string_set : field(default_factory = list)
+    preferred_strings : field(default_factory = list)
+    fitness : int = field(default = 0)
+
+
+
+
+if __name__ == "__main__":
+    #Set of nosj strings
+    foo = ["{A:B}", "{B:C}"]
+
+    #Example initialization of set class with the nosj strings
+    example_set = fuzzer_set(string_set = foo, preferred_strings = [foo[0]])
+    #Example of how to manually set fitness. Can be done with any variable.
+    example_set.fitness = 20
+
+    print(example_set)
+    print(example_set.string_set)
+    print(example_set.preferred_strings)
+
+    #Example of how to make a string set with params as a dict
+    params = {"fitness" : 10, "string_set" : foo, "preferred_strings" : [foo[1]]} 
+    example_set_2 = fuzzer_set(**params)
+    print(example_set_2)
+    #You can also print it as justa dict as well. 
+    print(asdict(example_set))
