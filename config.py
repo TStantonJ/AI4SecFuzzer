6d0d9b762a3e7b71c2f7210039cbcbd4c8c5a1e4
ThomasRStanton <thomasrstantonjr@gmail.com>
2022-04-28 18:27:26 -0500

    Added new selection config variables

--- config.py
+++ config.py
@@ -8,6 +8,8 @@ config = \
 "STRINGS_PER_SET" : 10,
 #PERCENT OF POPULATION THAT STAYS
 "PERCENT_SELECTED" : 80,
+"TOURNAMENT_SIZE" : 3,
+"PARENT_SELECTION_SIZE" : 25,
 #Amount of the population selected to win(For selection)
 "SELECTION_SIZE" : 10,
 "NUMBER_OF_CHILDREN" : 25,
